37f9604e19e63b76a4e3a26596e5930dfd732d26
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2022-12-21 17:39:56 +0000

    feat: added new feature

--- src/pytestpackage/calc_areas.py
+++ src/pytestpackage/calc_areas.py
@@ -30,7 +30,7 @@ def calc_volume_cube(x, y, z):
     Args:
         x (float): width of a cube
         y (float): length of a cube
-        z (float): depth of a cubee
+        z (float): depth of a cube
 
     Raises:
         TypeError: Type Error when width is inputted as a string
