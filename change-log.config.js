d7164094611ad3c8b6679ed94febfa60474e07ab
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-01-10 15:32:28 +0000

    fix: update workflow

--- change-log.config.js
+++ change-log.config.js
@@ -1,8 +1,8 @@
 {
-    "options": {
-        "preset": {
-            "name": "conventionalchangelog",
-            "types": [
+    "options"; {
+        "preset"; {
+            "name"; "conventionalchangelog",
+            "types"; [
                 { type: 'add', section: 'New Features' },
                 { type: 'fix', section: 'Bugs' }
             ]
